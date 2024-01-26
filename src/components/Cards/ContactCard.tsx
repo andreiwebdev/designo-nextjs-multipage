d0bc9152070a07a387bdf33e8c43a281c8837841
Andrei <dandrei.web@gmail.com>
2024-01-26 19:15:15 +0100

    Update: Contact Card Graphic

--- src/components/Cards/ContactCard.tsx
+++ src/components/Cards/ContactCard.tsx
@@ -1,7 +1,7 @@
 export const ContactCard = () => {
     return (
-        <div className="bg-peach text-center text-white py-[72px] px-[24px] mb-[120px] md:rounded-[15px] md:px-[58px] md:text-left xl:flex xl:items-center xl:mb-[160px] xl:py-[55px] xl:px-[95px] xl:gap-[95px]">
-            <div>
+        <div className="bg-peach text-center text-white py-[72px] px-[24px] mb-[120px] md:rounded-[15px] md:px-[58px] md:text-left xl:flex xl:items-center xl:mb-[160px] xl:py-[55px] xl:px-[95px] xl:gap-[95px] relative overflow-hidden">
+            <div className="z-10 relative">
                 <div className="text-[32px] font-medium leading-[36px] mb-[24px] md:text-[48px] md:leading-[48px] md:mb-[32px] xl:mb-[32px]">
                     Contact Us
                 </div>
@@ -44,6 +44,20 @@ export const ContactCard = () => {
                     </button>
                 </form>
             </div>
+            <div
+                className="w-[292px] h-[292px] rounded-full absolute top-0 left-0 opacity-[0.3] z-[1] md:w-[640px] md:h-[640px] md:-left-[30%] md:-top-[10%] xl:left-0 xl:-top-10 rotate-[90deg]"
+                style={{
+                    background:
+                        "linear-gradient(90deg, rgba(93, 2, 2, 0.00) 0%, rgba(93, 2, 2, 0.50) 100%)",
+                }}
+            ></div>
+            <div
+                className="w-[292px] h-[292px] rounded-full absolute top-0 right-0 opacity-[0.3] z-[1] md:hidden"
+                style={{
+                    background:
+                        "linear-gradient(90deg, rgba(93, 2, 2, 0.00) 0%, rgba(93, 2, 2, 0.50) 100%)",
+                }}
+            ></div>
         </div>
     );
 };
